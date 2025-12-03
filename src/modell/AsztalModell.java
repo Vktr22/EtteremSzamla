f1d87e21b4a9c68237251e71805050cb6939ff4d
Vktr22 <nds.viktoria@gmail.com>
2025-12-03 15:18:53 +0100

    felesleges setterek kiszedve etelModell-bol + AsztalModell(asztalonkenti penz osszesitessel)

--- src/modell/AsztalModell.java
+++ src/modell/AsztalModell.java
@@ -1,7 +1,37 @@
 
 package modell;
 
+import java.util.List;
+
 
 public class AsztalModell {
     private String szin;
+    private List<EtelModell> etelek;
+
+    public AsztalModell(String szin, String etelNev) {
+        this.szin = szin;
+        this.etelek = etelek;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+
+    
+    public List<EtelModell> getEtelek() {
+            return etelek;
+    }
+    
+    
+    public int fogyasztasOsszegeAsztalonkent(){
+        
+        int osszeg = 0;
+        
+        for (EtelModell e : etelek) {
+            osszeg += e.getAr();
+        }
+        return osszeg;
+
+
+    }
 }
--- src/modell/EtelModell.java
+++ src/modell/EtelModell.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modell;
 
-/**
- *
- * @author NádasViktóriaMária(S
- */
+
 public class EtelModell {
+    private String etelNev;
+    private int ar;
+    
+    public EtelModell(String etelNev){
+        this.etelNev = etelNev;
+    }
+    
+    public EtelModell(String etelNev, int ar){
+        this.etelNev = etelNev;
+        this.ar = ar;
+    }
+
+    public String getEtelNev() {
+        return etelNev;
+    }
+
+    public int getAr() {
+        return ar;
+    }
     
 }
