9f137802e4491a7b288c402074b38bc71b43fefa
Vktr22 <nds.viktoria@gmail.com>
2025-12-03 16:19:19 +0100

    szégyen

--- src/controller/SzamlaVezerlo.java
+++ src/controller/SzamlaVezerlo.java
@@ -13,6 +13,8 @@ public class SzamlaVezerlo {
 
     public static void main(String[] args) {
         List<EtelModell> etelekLista = new ArrayList<>();
+        List<AsztalModell> asztalok = new ArrayList<>();
+        
         String etelekFileUtvonal = "src/etelek.txt";
         String asztalokFileUtvonal = "src/asztalok.txt";
 
@@ -33,6 +35,43 @@ public class SzamlaVezerlo {
             System.out.println("Hiba:a fájl nem található!");
         }
         
+
+        try {
+            Scanner sc = new Scanner(new File(asztalokFileUtvonal));
+            String asztalSzinNev = null;
+            List<EtelModell> etelekAktualisAsztalhoz = new ArrayList<>();
+
+            while (sc.hasNextLine()) {
+                String sor = sc.nextLine().trim();
+                if (!sor.isEmpty()) {
+                    if (asztalSzinNev == null) {
+                        asztalSzinNev = sor;
+                    } else {
+                        
+                        for (EtelModell etel : etelekLista) {
+                            if (etel.getEtelNev().equals(sor)) {
+                                etelekAktualisAsztalhoz.add(etel);
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    
+                    asztalok.add(new AsztalModell(asztalSzinNev, etelekAktualisAsztalhoz));
+                    asztalSzinNev = null;
+                    etelekAktualisAsztalhoz = new ArrayList<>();
+                }
+            }
+            
+            if (asztalSzinNev != null) {
+                asztalok.add(new AsztalModell(asztalSzinNev, etelekAktualisAsztalhoz));
+            }   //mivel üres soronként adja hozzá, ha az utsó asztal utsó kaja után nincs új üres sor, akk is adja hozzá az asztal nevét+ételeit
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Hiba: asztalok.txt nem található!");
+        }
+
         
+       //SzamlaCUInezet.kiir(asztalok);
     }
 }
--- src/nezet/SzamlaCUInezet.java
+++ src/nezet/SzamlaCUInezet.java
@@ -1,7 +1,21 @@
 
 package nezet;
 
+import modell.AsztalModell;
+import modell.EtelModell;
+import java.util.List;
 
 public class SzamlaCUInezet {
-    
+
+    public static void kiir(List<AsztalModell> asztalok) {
+        for (AsztalModell asztal : asztalok) {
+            System.out.println(asztal.getSzin() + " asztal:");
+            System.out.println("========================");
+            for (EtelModell etel : asztal.getEtelek()) {
+                System.out.printf("%-20s %d Ft%n", etel.getEtelNev(), etel.getAr());
+            }
+            System.out.println("------------------------");
+            System.out.println("Összesen: " + asztal.fogyasztasOsszegeAsztalonkent() + " Ft\n");
+        }
+    }
 }
