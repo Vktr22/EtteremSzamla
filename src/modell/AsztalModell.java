b303af91599ad8d5d9ec54f3087060b4506cd33f
Vktr22 <nds.viktoria@gmail.com>
2025-12-03 14:48:37 +0100

    EtelModell

--- src/modell/AsztalModell.java
+++ src/modell/AsztalModell.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modell;
 
-/**
- *
- * @author NádasViktóriaMária(S
- */
+
 public class AsztalModell {
-    
+    private String szin;
 }
